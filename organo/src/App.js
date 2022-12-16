5eb6232e0033341c0d5322726939b05c84994d6d
Luana <santosmeiraluana@gmail.com>
2022-12-16 16:51:13 -0300

    alinhando e dando forma ao formulario

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,14 +1,11 @@
 import Banner from './componentes/banner/banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulário';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite seu nome" />
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-
+      <Formulario />
     </div>
   );
 }
